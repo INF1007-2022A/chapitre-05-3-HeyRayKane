d2f007a6a8e9a99cf175bd98b90edcebfecb2c38
HeyRayKane <rayan.ajakane@polymtl.ca>
2022-10-17 21:30:43 -0400

    complétion de l'exercice 5.3

--- exercice.py
+++ exercice.py
@@ -26,27 +26,43 @@ def get_word_length_histogram(text):
 			if len(list[j]) > b:
 				b = len(list[j])
 		elif "-" in list[j]:
-			histogram[len(list[j])-1] += 1
-			if len(list[j])-1 > b:
-				b = len(list[j])-1
+			a=list[j].count("-")
+			histogram[len(list[j])-a] += 1
+			if len(list[j])-a > b:
+				b = len(list[j])-a
 	histogramf =histogram[:b+1]
 	return histogramf
-"""
+
 def format_histogram(histogram):
 	ROW_CHAR = "*"
-
-	return ""
+	vertical = ""
+	for i in range(1,len(histogram)):
+		vertical+= f"\n{i:>2} {ROW_CHAR * histogram[i]}"
+	return vertical
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
 	LINE_CHAR = "¯"
+	horizontal = LINE_CHAR*(len(histogram))
+	for i in range(1,max(histogram)+1):
+		liste = [""] * (len(histogram))
+		b=0
+		a = ""
+		for j in histogram[1:]:
+			if j>=i:
+				liste[b] = BLOCK_CHAR
+			else:
+				liste[b] = " "
+			b += 1
+		for k in liste:
+			a+=k
+		horizontal = a+chr(10)+horizontal
+	return horizontal
 
-	return ""
-"""
 
 if __name__ == "__main__":
 	spam = "Stop right there criminal scum! shouted the guard confidently."
 	eggs = get_word_length_histogram(spam)
 	print(eggs, "\n")
-	#print(format_histogram(eggs), "\n")
-	#print(format_horizontal_histogram(eggs))
+	print(format_histogram(eggs), "\n")
+	print(format_horizontal_histogram(eggs))
